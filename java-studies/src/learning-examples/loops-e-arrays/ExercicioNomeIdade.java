4fedf26b1b4229bedef021f0ef312438af0a7e75
anelymr <mylenarocha@live.com>
2025-04-06 16:52:17 -0300

    Adiciona o primeiro exercicio utilizando while

---
+++ java-studies/src/learning-examples/loops-e-arrays/ExercicioNomeIdade.java
@@ -0,0 +1,29 @@
+/*
+ Faça um programa que leia conjuntos de dois valores;
+ 1. representando o nome do aluno;
+ 2. representando a idade do aluno;
+ 3. pare o programa, inserindo o valor 0 no campo nome;
+*/
+
+import java.util.Scanner;
+
+public class ExercicioNomeIdade {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String  nome;
+        int     idade;
+
+        while (true) {
+            System.out.println("Nome: ");
+                nome = scan.next();
+                if (nome.equals("0")) break;
+
+            System.out.println("Idade: ");
+                idade = scan.nextInt();
+        }
+        
+        System.out.println("Você encerrou o processo");
+
+    }
+}
