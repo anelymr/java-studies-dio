18e602dad741f71aae26fc0115b8a355be27a806
anelymr <mylenarocha@live.com>
2025-04-07 17:06:40 -0300

    Adiciona um exercicio de array, onde um vetor lê uma quantidade de consoantes e  as imprime em tela

--- java-studies/src/ExerciciosConsoantes.java
+++ java-studies/src/ExerciciosConsoantes.java
@@ -5,8 +5,37 @@
  3. Imprima as consoantes.
  */
 
+import java.util.Scanner;
+
 public class ExerciciosConsoantes {
     public static void main(String[] args) {
-        
+        Scanner scan = new Scanner(System.in);
+
+        String[] consoantes = new String[6];
+        int qtd_Consoantes = 0;
+        int count = 0;
+
+        do {
+            System.out.println("Letra: ");
+            String letra = scan.next();
+
+            if (!(letra.equalsIgnoreCase("a")
+                    || letra.equalsIgnoreCase("e")
+                    || letra.equalsIgnoreCase("i")
+                    || letra.equalsIgnoreCase("o")
+                    || letra.equalsIgnoreCase("u"))) {
+                consoantes[count] = letra;
+                qtd_Consoantes++;
+            };
+
+            count++;
+        } while (count < consoantes.length);
+
+        System.out.println("Consoantes: ");
+        for (String consoante : consoantes) {
+            if(consoante != null)
+                System.out.println(consoante);
+        }
+        scan.close();
     }
 }
